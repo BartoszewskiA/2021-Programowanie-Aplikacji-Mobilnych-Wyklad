b535c733b02f3b3294b3f55a388de032640da14f
BartoszewskiA <bartoszewski.a@outlook.com>
2021-11-27 17:22:50 +0100

    27.11.2021

---
+++ w05p01-aktywnosci/app/src/main/java/com/example/w05p01_aktywnosci/MainActivity3.java
@@ -0,0 +1,14 @@
+package com.example.w05p01_aktywnosci;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+}
